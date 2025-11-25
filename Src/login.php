a2b51cfcb67c26788d3453b11d9b357cf02dbdd8
dy6P <peronidylan06@gmail.com>
2025-11-25 19:13:30 +0100

    Ajout des dossiers Src et Documentation. Intégration de la fonctionnalité suivante : Connexion des utilisateurs simple (sans les fonctionnalités associées).

---
+++ Src/login.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+
+$conn = mysqli_connect("localhost", "root", "", "Parc_informatique");
+
+if (!$conn) {
+    die("Connexion échouée : " . mysqli_connect_error());
+}
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $user = $_POST['username'];
+    $pass = $_POST['password'];
+
+    $sql = "SELECT password FROM Users WHERE user =". $user;
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $row = mysqli_fetch_assoc($result);
+        if ($pass === $row['password']) {
+            $_SESSION['user'] = $user;
+
+            if ($user == "technicien") {
+                header("Location: technicien.html");
+            } else {
+                header("Location: index.html");
+            }
+            exit;
+        } else {
+            echo "Mot de passe incorrect.";
+        }
+    } else {
+        echo "Utilisateur non trouvé.";
+    }
+}
+
+mysqli_close($conn);
+
